c0b2af7275e9824d95d905d4f1e86518ac292601
Farhanunnasih <farhanunnasih@gmail.com>
2024-04-28 19:47:11 +0700

    Init

--- routes/api.php
+++ routes/api.php
@@ -8,10 +8,6 @@ use App\Http\Middleware\AlwaysAcceptJson;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::post('/login', LoginController::class);
 Route::post('/register', RegisterController::class);
 
